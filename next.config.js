5e849ec15a23bade8487f7bdde35c286a577866f
ianfebi01 <ianfebi01@gmail.com>
2023-05-03 01:48:56 +0700

    change api

--- next.config.js
+++ next.config.js
@@ -2,19 +2,19 @@
 const nextConfig = {
   reactStrictMode: false,
 
-  async rewrites() {
-    return [
-      {
-        source: "/api-web/:path*",
-        destination: process.env.BASE_URL,
-      },
-      {
-        source: "/getActivity",
-        destination:
-          "https://todo.api.devcode.gethired.id/activity-groups?email=ianfebi01%40gmail.com",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api-web/:path*",
+  //       destination: process.env.BASE_URL,
+  //     },
+  //     {
+  //       source: "/getActivity",
+  //       destination:
+  //         "https://todo.api.devcode.gethired.id/activity-groups?email=ianfebi01%40gmail.com",
+  //     },
+  //   ];
+  // },
 
   webpack(config) {
     config.module.rules.push({
